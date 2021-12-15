597257292176b5901c312196b1c898fa591f9963
hex <rafis-tatar@rambler.ru>
2021-12-15 16:04:03 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace Auth.ADExport
     {
         static void Main(string[] args)
         { 
-            var users = ADUserService.GetAllUsers("tatneft.ru", "hasanovaz@hq.tatneft.ru", "4rfv$RFV");
+            var users = ADUserService.GetAllUsers("<root domain>", "<domain user name>", "<password>");
             var databaseService = new DatabaseService();
             databaseService.Export(users);
             //var str = string.Join("; \n\n", users.Select(u => $"AccountFullName:{u.AccountFullName}, AccountName:{u.AccountName}, UserName:{u.UserName}, Title:{u.Title}, Company:{u.Company}, Path:{u.Path}, UserDept:{u.UserDept}"));
